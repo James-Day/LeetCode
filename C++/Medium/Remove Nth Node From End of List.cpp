e34a9182b8e5c2ee88a45da97cefb5aa6ffd1cbc
James-Day <85190098+James-Day@users.noreply.github.com>
2023-11-20 11:01:59 -0800

    New Solution This solution is alot more clean and is much faster. I still need to delete the nodes to remove memory leak.

--- C++/Medium/Remove Nth Node From End of List.cpp
+++ C++/Medium/Remove Nth Node From End of List.cpp
@@ -1,45 +1,30 @@
-
-// Definition for singly-linked list.
-  struct ListNode {
-      int val;
-      ListNode *next;
-      ListNode() : val(0), next(nullptr) {}
-      ListNode(int x) : val(x), next(nullptr) {}
-      ListNode(int x, ListNode *next) : val(x), next(next) {}
-  };
- 
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int size = 0;
-        ListNode* copy = head;
-        ListNode* returnCopy = head;
-        ListNode* returnCopy2 = head;
-        while (head != nullptr) {
-            head = head->next;
-            size++;
-        }
-        int nodeToDelete = size - n;
-        while (nodeToDelete > 0) {
-            copy = copy->next;
-            nodeToDelete--;
-        }
+        ListNode* right = head;
+        ListNode* left = head;
 
-        if (copy->next != nullptr) {
-            copy->val = copy->next->val;
-            copy->next = copy->next->next;
+        for (int i = 0; i < n; i++) {
+            right = right->next;
         }
-        else {
-            size--;
-            if (size == 0) {
-                return nullptr;
-            }
-            while (returnCopy2->next != copy) {
-                returnCopy2 = returnCopy2->next;
-            }
-            returnCopy2->next = nullptr;
-            delete copy;
+        if (right == nullptr) {
+            return left->next; ///would obviously need to delete original node
         }
-        return returnCopy;
+        while (right->next) {
+            right = right->next;
+            left = left->next;
+        }
+        left->next = left->next->next;//would need to delete left->next
+        return head;
     }
 };
