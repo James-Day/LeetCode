18be5d95a0f14b6822a172f9b0eda27cbd7a29e7
James-Day <85190098+James-Day@users.noreply.github.com>
2023-12-01 13:43:10 -0800

    Advent of Code Day #1
    This was a really good question for the first day!, part one allowed me
    to get used to opening files with C++, and part 2 actually gave me a bit
    of a challenge! (I assume my code is not optimal, as I believe it's like
    O(n^3)) or so.

---
+++ C++/AdventOfCode/1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+struct numToNum {
+    std::string str;
+    int num;
+};
+
+const int NUMDIGITS = 10;
+numToNum lookUp[] = {
+    {"one", 1},
+    {"two", 2},
+    {"three", 3},
+    {"four", 4},
+    {"five", 5},
+    {"six", 6},
+    {"seven", 7},
+    {"eight", 8},
+    {"nine", 9}
+};
+
+int isNum(std::string input) {
+    for (int i = 0; i < NUMDIGITS; i++) {
+        if (lookUp[i].str == input) {
+            return i + 1;
+        };
+    }
+    return -1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        printf("Usage: %s <filename>\n", argv[0]);
+        exit(-1);
+    }
+    std::ifstream input(argv[1]);
+    std::string line;
+
+    if (!input.is_open()) {
+        std::cout << "Failed to open input file \n";
+        exit(-1);
+    }
+    int sum = 0;
+    while (std::getline(input, line)) {
+        std::string leftMost = "";
+        std::string rightMost = "";
+        for (int i = 0; i < line.size(); i++) {
+            if (leftMost != "") break;
+            if (isdigit(line[i])) {
+                leftMost += line[i];
+                break;
+            }
+            for (int j = 0; j < i; j++) {
+                if (isNum(line.substr(j, i - j + 1)) != -1) {
+                    leftMost += std::to_string(isNum(line.substr(j, i - j + 1)));
+                    break;
+                }
+            }
+        }
+
+        std::cout << leftMost << " ";
+        for (int i = line.size() - 1; i >= 0; i--) {
+            if (rightMost != "") break;
+            if (isdigit(line[i])) {
+                rightMost += line[i];
+                break;
+            }
+            for (int j = 0; j < line.size() - i; j++) {
+                if (isNum(line.substr(i, line.size() - i - j)) != -1) {
+                    rightMost += std::to_string(isNum(line.substr(i, line.size() - i - j)));
+                    break;
+                }
+            }
+        }
+        std::cout << rightMost << std::endl;
+        sum += std::stoi(leftMost + rightMost);
+    }
+
+    std::cout << sum << std::endl;
+
+    input.close();
+
+}
