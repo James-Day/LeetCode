85e87b762473e972c65a1b57376053961af4dc37
James-Day <85190098+James-Day@users.noreply.github.com>
2024-03-22 16:53:06 -0700

    3/22 Daily Leetcode (plan to bring back my streak of dailies)

--- C++/Easy/Palindrome Linked List.cpp
+++ C++/Easy/Palindrome Linked List.cpp
@@ -8,7 +8,7 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-class Solution { //I'll definitally come back and do a proper solution, a stack should probablly be used instead of a vector as well. //can be done in O(1) space by reversing the second half of the list
+/*class Solution { //I'll definitally come back and do a proper solution, a stack should probablly be used instead of a vector as well. //can be done in O(1) space by reversing the second half of the list
 public:
     bool isPalindrome(ListNode* head) {
         
@@ -38,5 +38,50 @@ public:
         }
         return true;
         
+    }
+};*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution { //reverse the second half of the list so that it is O(n) time and O(1) space! :)
+public:
+    bool isPalindrome(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+        int size = 0;
+        while (fast != nullptr && fast->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            size++;
+        }
+        ListNode* rev = reverse(slow);
+
+        while (rev != nullptr) {
+            if (head->val != rev->val) {
+                return false;
+            }
+            head = head->next;
+            rev = rev->next;
+        }
+        return true;
+    }
+    ListNode* reverse(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        while (curr != nullptr) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 };
