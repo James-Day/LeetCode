ab09a8ee232c7d2b2aba6d4447c1ef23db46f673
James-Day <james-day1@live.com>
2024-04-17 11:46:48 -0700

    cleaned up Balanced Binary Tree (LC 110)

--- C++/Easy/Balanced Binary Tree.cpp
+++ C++/Easy/Balanced Binary Tree.cpp
@@ -9,6 +9,32 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+ /**
+  * Definition for a binary tree node.
+  * struct TreeNode {
+  *     int val;
+  *     TreeNode *left;
+  *     TreeNode *right;
+  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  * };
+  */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return root == nullptr ? true : height(root) != -1;
+    }
+    int height(TreeNode* root) {
+        if (!root) return 0;
+        int l = height(root->left);
+        int r = height(root->right);
+        if (l == -1 || r == -1) return -1; //once there is a height imbalance, return the way up the stack.
+        if (abs(l - r) > 1) return -1;
+        return max(l, r) + 1;
+    }
+};
+/*
 #include <algorithm>
 using namespace std;
 class Solution {
@@ -35,3 +61,4 @@ public:
       return max(l,r) + 1;
     }
 };
+*/
