f2240d73aa14567c2b1266393b23b6876a0ed607
James-Day <james-day1@live.com>
2024-04-30 15:11:13 -0700

    comments

--- C++/Medium/Minimum Number of Operations to Make Array XOR Equal to K.cpp
+++ C++/Medium/Minimum Number of Operations to Make Array XOR Equal to K.cpp
@@ -6,8 +6,8 @@ public:
             finalXorVal ^= n;
         }
         int count = 0;
-        while (k || finalXorVal) {
-            if (k % 2 != finalXorVal % 2) {
+        while (k || finalXorVal) { //until both are zero check each bit
+            if (k % 2 != finalXorVal % 2) { // 1 , 0 , or 0, 1 (i.e. bits are different)
                 count++;
             }
             k /= 2;
