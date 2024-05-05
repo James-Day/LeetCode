b861c386c09e42016e489f87aaed7df9cda131ee
James-Day <85190098+James-Day@users.noreply.github.com>
2024-05-05 14:26:40 -0700

    Rework

--- C++/Medium/Minimum Length of Anagranm Concatenation.cpp
+++ C++/Medium/Minimum Length of Anagranm Concatenation.cpp
@@ -1,5 +1,38 @@
-//This solution passes all 550 test cases but it's actually wrong. For example "aabbaabb" We would get an answer of 8, but the true answer is 4. I believe the true answer has to do with factors, I will try and come back and do this tomorrow
+//better solution, passes edge cases even though leetcode doesn't require it.
 class Solution {
+public:
+    int ok(string& s, int k) {
+        int n = s.length();
+        int cnt[26] = { 0 }; // better than my original map because of overhead
+        for (int i = 0; i < k; i++) {
+            cnt[s[i] - 'a']++;
+        }
+        for (int i = k; i < n; i += k) {
+            int cnt2[26] = { 0 };
+            for (int j = i; j < i + k; j++) {
+                cnt2[s[j] - 'a']++;
+            }
+            for (int j = 0; j < 26; j++) {
+                if (cnt[j] != cnt2[j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    int minAnagramLength(string s) {
+        int n = s.size();
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0 && ok(s, i)) {
+                return i;
+            }
+        }
+        return n;
+    }
+};
+
+//This solution passes all 550 test cases but it's actually wrong. For example "aabbaabb" We would get an answer of 8, but the true answer is 4. I believe the true answer has to do with factors, I will try and come back and do this tomorrow
+/*class Solution {
 public:
     int minAnagramLength(string s) {
         unordered_map<char, int> count;
@@ -39,3 +72,4 @@ public:
     }
 
 };
+*/
