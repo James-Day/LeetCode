885cd4481dc60f2ebe87d553e382e13c33eca8cb
James-Day <85190098+James-Day@users.noreply.github.com>
2024-05-06 18:59:21 -0700

    cleanup

--- C++/Medium/Number of Islands.cpp
+++ C++/Medium/Number of Islands.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
-        int ans = 0;
+        int res = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[i].size(); j++) {
-                if (grid[i][j] != '1') {
-                    continue;
+                if (grid[i][j] == '1') {
+                    floodFill(grid, i, j);
+                    res++;
                 }
-                floodFill(grid, i, j);
-                ans++;
             }
         }
-        return ans;
+        return res;
     }
     void floodFill(vector<vector<char>>& grid, int i, int j) {
         if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size()) return;
-        if (grid[i][j] != '1') return;
-        grid[i][j] = 0;
+        if (grid[i][j] == '0') return;
+        grid[i][j] = '0';
         floodFill(grid, i + 1, j);
         floodFill(grid, i - 1, j);
         floodFill(grid, i, j + 1);
