052150984ec056657789ea5d4e4c372565b8df5c
James-Day <85190098+James-Day@users.noreply.github.com>
2024-05-23 13:04:06 -0700

    Daily LC 5/22 (Optimized solution!)

--- C++/Medium/The Number of Beautiful Subsets.cpp
+++ C++/Medium/The Number of Beautiful Subsets.cpp
@@ -23,3 +23,52 @@ public:
     }
 
 };
+
+//O(n)
+class Solution {
+public:
+    unordered_map<int, int> cache;
+    int beautifulSubsets(vector<int>& nums, int k) {
+        unordered_map<int, int> masterCount;
+        for (int num : nums) {
+            masterCount[num]++;
+        }
+        vector<unordered_map<int, int>> groups;
+        unordered_set<int> visited;
+        for (auto i = masterCount.begin(); i != masterCount.end(); i++) {
+            if (visited.contains(i->first)) continue; //visited this key already.
+
+            unordered_map<int, int> g;
+            int n = i->first;
+            while (masterCount.contains(n - k)) {
+                n -= k; //could optimize this a tiny bit
+            }
+            //n is the minimum value
+            while (masterCount.contains(n)) {
+                g[n] = masterCount[n];
+                visited.insert(n);
+                n += k;
+            }
+            groups.push_back(g);
+        }
+        int res = 1;
+        for (unordered_map<int, int>& g : groups) {
+            int minimumKey = INT_MAX;
+            for (auto i = g.begin(); i != g.end(); i++) {
+                minimumKey = min(minimumKey, i->first);
+            }
+            res *= helper(minimumKey, g, masterCount, k);
+        }
+        return res - 1;
+
+    }
+    int helper(int n, unordered_map<int, int>& g, unordered_map<int, int> count, int& k) {
+        if (!g.contains(n))
+            return 1;
+        if (cache.contains(n)) return cache[n]; //a number can only be in one group so we don't have to worry about the group changing.
+        int skip = helper(n + k, g, count, k);
+        int include = (pow(2, g[n]) - 1) * helper(n + (2 * k), g, count, k); //you have to skip the next k value because you can't do 2 in a row
+        cache[n] = skip + include;
+        return skip + include;
+    }
+};
