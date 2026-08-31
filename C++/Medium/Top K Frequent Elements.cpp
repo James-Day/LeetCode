9fc79187a3aa64ffc8081f51c8fe95e415da1565
James-Day <james-day1@live.com>
2026-08-30 18:52:56 -0700

    Update 347. Top K Frequent Elements - C++ (add bucket, heap, sorting solutions)

--- C++/Medium/Top K Frequent Elements.cpp
+++ C++/Medium/Top K Frequent Elements.cpp
@@ -2,6 +2,7 @@
 #include <unordered_map>
 using namespace std;
 
+//best time complexity solution (buckets)
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
@@ -28,3 +29,58 @@ public:
         return ans;
     }
 };
+
+//sorting solution
+/*
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int ,int> count;
+
+        for(int i = 0; i <nums.size(); i++){
+            count[nums[i]] ++;
+        }
+        vector<vector> freq;
+
+        for(auto it = count.begin(); it != count.end(); it++){
+            freq.push_back({it->second, it->first});
+        }
+
+        sort(freq.rbegin(), freq.rend());
+        vector<int> res;
+        for(int i = 0; i < k; i++){
+            res.push_back(freq[i].second);
+        }
+        return res;
+    }
+};
+*/
+
+//heap solution
+/*
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> count;
+        for (int num : nums) {
+            count[num]++;
+        }
+
+        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> heap;
+        for (auto& entry : count) {
+            heap.push({entry.second, entry.first});
+            if (heap.size() > k) {
+                heap.pop();
+            }
+        }
+
+        vector<int> res;
+        for (int i = 0; i < k; i++) {
+            res.push_back(heap.top().second);
+            heap.pop();
+        }
+        return res;
+    }
+};
+*/
