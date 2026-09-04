fb1f91dab211a7a2527bcd4b880b76c9eec8d002
James-Day <james-day1@live.com>
2026-09-03 18:53:52 -0700

    Update 238. Product of Array Except Self - C++ (add division solution)

--- C++/Medium/Product of Array Except Self.cpp
+++ C++/Medium/Product of Array Except Self.cpp
@@ -1,5 +1,8 @@
+// https://leetcode.com/problems/product-of-array-except-self/
 #include <vector>
 using namespace std;
+
+// Prefix & Suffix product solution (O(1) extra space, no division)
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
@@ -18,3 +21,40 @@ public:
         return ans;
     }
 };
+
+// Division with zero-handling solution
+/*
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        if(nums.empty()) return {};
+
+        int zeroCount = 0;
+        int totalWithoutZero = 1;
+
+        for(int num: nums){
+            if (num == 0) {
+                zeroCount++;
+            } else {
+                totalWithoutZero *= num;
+            }
+        }
+
+        vector<int> ans(nums.size(), 0);
+
+        // return all zeros if more than 2 zeros
+        if (zeroCount >= 2) {
+            return ans;
+        }
+
+        for(int i = 0; i < nums.size(); i++){
+             if (nums[i] == 0) {
+                ans[i] = totalWithoutZero;           
+            } else if (zeroCount == 0) {
+                ans[i] = totalWithoutZero / nums[i]; //can't be zero, so division is fine
+            }
+        }
+        return ans;
+    }
+};
+*/
